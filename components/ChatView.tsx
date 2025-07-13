7702a3a0213da4a0f78b41a6835d1d782d163502
Worldtour Rafi <85597360+rafi2225@users.noreply.github.com>
2025-07-13 23:53:44 +0700

    Fix footer JSX structure by moving it inside main div

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -138,6 +138,8 @@ const ChatView: React.FC<ChatViewProps> = ({
         )}
       </main>
 
+      <footer
+        className="fixed bottom-0 bg-white dark:bg-[#1c1c1c] z-10 p-4 md:p-6 pb-0 md:pb-0 left-0 right-0 w-full"
         style={{
           ...(window.innerWidth >= 768 && {
             left: `${sidebarWidth}px`,
@@ -150,10 +152,6 @@ const ChatView: React.FC<ChatViewProps> = ({
           <ChatInput 
             onMessageSend={handleSendMessage}
             disabled={streamingState.isStreaming || isLoading}
-          />
-        </div>
-      </footer>
-    </div>
   );
 };
 
