41042890a00186b3380e005cffdce5e9d8d5c1e0
Worldtour Rafi <85597360+rafi2225@users.noreply.github.com>
2025-07-13 22:28:36 +0700

    Fix React import in useChat hook

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useCallback, useRef } from 'react';
 import { ChatMessage, ChatConversation, StreamingState } from '../types/chat';
 import { ChatService } from '../services/chatService';
